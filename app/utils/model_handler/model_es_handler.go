723dbae21cd4057233e8d69d718ab6b8a5d649dd
Havoc412 <2993167370@qq.com>
2024-11-14 01:42:50 +0800

    🖊️

--- app/utils/model_handler/model_es_handler.go
+++ app/utils/model_handler/model_es_handler.go
@@ -1,5 +1,10 @@
 package model_handler
 
+/**
+ * @description: 用于处理 ES-highlight 模块分析出来的 []String.
+ * @param {[]interface{}} strs
+ * @return {*}
+ */
 func TransStringSliceToString(strs []interface{}) string {
 	var result string
 	for _, str := range strs {
