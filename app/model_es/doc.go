e636bbb9c2f7051cf97cd4708523fbdd835c37b0
Havoc412 <2993167370@qq.com>
2024-11-15 22:15:47 +0800

    fit to docx

--- app/model_es/doc.go
+++ app/model_es/doc.go
@@ -1,12 +1,13 @@
 package model_es
 
-// INFO @brief 这个文件就是处理 ES 存储文档特征向量的集中处理
+// INFO @brief 这个文件就是处理 ES 存储文档特征向量的集中处理；
 
 func CreateDocESFactory() *Doc {
 	return &Doc{}
 }
 
 type Doc struct {
+	Id        int64     `json:"id"` // 对应 MySQL 中存储的文档 ID。
 	Content   string    `json:"content"`
 	Embedding []float64 `json:"embedding"`
 }
@@ -15,6 +16,7 @@ func (d *Doc) IndexName() string {
 	return "catface_docs"
 }
 
-func (d *Doc) InsertDocument() error {
-	return nil
-}
+// UPDATE 因为 chunck 还是 Python 来处理会比较方便，所以 Go 这边主要还是处理查询相关的操作。
+// func (d *Doc) InsertDocument() error {
+// 	return nil
+// }
