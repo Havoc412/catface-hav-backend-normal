2fc1e2d8da50bf25d01219dac3ca60849e58d7ab
Havoc412 <2993167370@qq.com>
2024-11-18 00:40:32 +0800

    🖊️

--- app/global/errcode/nlp.go
+++ app/global/errcode/nlp.go
@@ -14,5 +14,5 @@ func NlpMsgInit(m msg) {
 func NlpMsgUserInit(m msg) {
 	m[ErrNoContent] = "请输入内容"
 	m[ErrNoDocFound] = "小护没有在知识库中找到相关文档。😿"
-	m[ErrPythonServierDown] = "小护的🐍python服务挂了，请稍后再试。😿"
+	m[ErrPythonServierDown] = "小护的🐍python服务挂了，此功能暂时无法使用。😿"
 }
